768f65d091b8086f16dd71579384bc443fa6ecee
Amit Patel <amitpatel28092001@gmail.com>
2024-01-09 01:52:04 +0530

    Initial Commit

---
+++ iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Amit Patel on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
